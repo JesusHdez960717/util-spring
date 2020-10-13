ae843afecf6267866eec6838633ae65311b0a4e8
Unknown <unknown@example.com>
2020-10-13 06:32:01 -0500

    +RESTUrlConstants

---
+++ src/main/java/com/jhw/utils/spring/server/RESTUrlConstants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.utils.spring.server;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class RESTUrlConstants {
+
+    public static final String PATH_TRABAJO_CREATE = "/create";
+    public static final String PATH_TRABAJO_EDIT = "/edit";
+    public static final String PATH_TRABAJO_DESTROY = "/destroy";
+    public static final String PATH_TRABAJO_DESTROY_ID = "/destroy_id";
+    public static final String PATH_TRABAJO_FIND_ALL = "/find_all";
+    public static final String PATH_TRABAJO_FIND_BY = "/find_by/{id}";
+    public static final String PATH_TRABAJO_COUNT = "/count";
+}
