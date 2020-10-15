2ca3fde6c8b4f7c9c9dbe37eac8c9db2f5f72f44
Unknown <unknown@example.com>
2020-10-15 16:17:43 -0500

    RESTUrlConstants: + method

--- src/main/java/com/jhw/utils/spring/server/RESTUrlConstants.java
+++ src/main/java/com/jhw/utils/spring/server/RESTUrlConstants.java
@@ -5,17 +5,44 @@
  */
 package com.jhw.utils.spring.server;
 
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
  */
 public class RESTUrlConstants {
 
-    public static final String PATH_TRABAJO_CREATE = "/create";
-    public static final String PATH_TRABAJO_EDIT = "/edit";
-    public static final String PATH_TRABAJO_DESTROY = "/destroy";
-    public static final String PATH_TRABAJO_DESTROY_ID = "/destroy_id";
-    public static final String PATH_TRABAJO_FIND_ALL = "/find_all";
-    public static final String PATH_TRABAJO_FIND_BY = "/find_by/{id}";
-    public static final String PATH_TRABAJO_COUNT = "/count";
+    public static final String CREATE_PATH = "/create";
+    public static final RequestMethod CREATE_METHOD = RequestMethod.POST;
+
+    public static final String CREATE_PATH_LIGHTWEIGHT = "/create_light";
+    public static final RequestMethod CREATE_LIGHTWEIGHT_METHOD = RequestMethod.PUT;
+
+    public static final String EDIT_PATH = "/edit";
+    public static final RequestMethod EDIT_METHOD = RequestMethod.POST;
+
+    public static final String EDIT_PATH_LIGHTWEIGHT = "/edit_light";
+    public static final RequestMethod EDIT_LIGHTWEIGHT_METHOD = RequestMethod.PUT;
+
+    public static final String DESTROY_PATH = "/destroy";
+    public static final RequestMethod DESTROY_METHOD = RequestMethod.POST;
+
+    public static final String DESTROY_PATH_LIGHTWEIGHT = "/destroy_light";
+    public static final RequestMethod DESTROY_LIGHTWEIGHT_METHOD = RequestMethod.DELETE;
+
+    public static final String DESTROY_ID_PATH = "/destroy_id";
+    public static final RequestMethod DESTROY_ID_METHOD = RequestMethod.POST;
+
+    public static final String DESTROY_ID_LIGHTWEIGHT_PATH = "/destroy_id";
+    public static final RequestMethod DESTROY_ID_LIGHTWEIGHT_METHOD = RequestMethod.DELETE;
+
+    public static final String FIND_ALL_PATH = "/find_all";
+    public static final RequestMethod DFIND_ALL_METHOD = RequestMethod.GET;
+
+    public static final String FIND_BY_PATH = "/find_by/{id}";
+    public static final RequestMethod FIND_BY_METHOD = RequestMethod.GET;
+
+    public static final String COUNT_PATH = "/count";
+    public static final RequestMethod COUNT_METHOD = RequestMethod.GET;
 }
