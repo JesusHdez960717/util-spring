c449cd4615dc632db5d3f70cf84502f3519018b0
Unknown <unknown@example.com>
2020-12-19 11:55:06 -0500

    ConsumerRepoTemplate, Light: +template()

--- src/main/java/com/jhw/utils/spring/client/ConsumerRepoTemplateLight.java
+++ src/main/java/com/jhw/utils/spring/client/ConsumerRepoTemplateLight.java
@@ -8,7 +8,6 @@ package com.jhw.utils.spring.client;
 import com.clean.core.app.repo.CRUDLightweightRepo;
 import com.clean.core.app.usecase.CRUDLightweightUseCase;
 import com.jhw.utils.spring.server.RESTUrlConstants;
-import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -16,32 +15,32 @@ import org.springframework.web.client.RestTemplate;
  * @param <Domain>
  */
 @Deprecated
-public class ConsumerRepoTemplateLight<Domain> extends ConsumerRepoTemplate<Domain> implements CRUDLightweightUseCase<Domain>, CRUDLightweightRepo<Domain> {
+public abstract class ConsumerRepoTemplateLight<Domain> extends ConsumerRepoTemplate<Domain> implements CRUDLightweightUseCase<Domain>, CRUDLightweightRepo<Domain> {
 
-    public ConsumerRepoTemplateLight(RestTemplate template, Class<? extends Domain> clazz, String urlGeneral) {
-        super(template, clazz, urlGeneral);
+    public ConsumerRepoTemplateLight(Class<? extends Domain> clazz, String urlGeneral) {
+        super(clazz, urlGeneral);
     }
 
     @Override
     public void create_light(Domain newObject) throws Exception {
-        template.put(urlGeneral + RESTUrlConstants.CREATE_PATH_LIGHTWEIGHT, newObject);
+        template().put(urlGeneral + RESTUrlConstants.CREATE_PATH_LIGHTWEIGHT, newObject);
     }
 
     @Override
     public void edit_light(Domain objectToUpdate) throws Exception {
-        template.put(urlGeneral + RESTUrlConstants.EDIT_PATH_LIGHTWEIGHT, objectToUpdate);
+        template().put(urlGeneral + RESTUrlConstants.EDIT_PATH_LIGHTWEIGHT, objectToUpdate);
     }
 
     @Override
     @Deprecated
     public void destroy_light(Domain objectToDestroy) throws Exception {
-        template.delete(urlGeneral + RESTUrlConstants.DESTROY_PATH, objectToDestroy);
+        template().delete(urlGeneral + RESTUrlConstants.DESTROY_PATH, objectToDestroy);
     }
 
     @Override
     @Deprecated
     public void destroyById_light(Object keyId) throws Exception {
-        template.delete(urlGeneral + RESTUrlConstants.DESTROY_ID_PATH, keyId);
+        template().delete(urlGeneral + RESTUrlConstants.DESTROY_ID_PATH, keyId);
     }
 
 }
